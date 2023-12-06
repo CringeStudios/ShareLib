4b168c015324d43e709bd0309358870da19c259c
MrLetsplay <mr@cringe-studios.com>
2023-12-06 21:13:21 +0100

    Add BROADCAST_SITE_ID

--- src/main/java/me/mrletsplay/shareclientcore/connection/message/AddressableMessage.java
+++ src/main/java/me/mrletsplay/shareclientcore/connection/message/AddressableMessage.java
@@ -2,6 +2,8 @@ package me.mrletsplay.shareclientcore.connection.message;
 
 public interface AddressableMessage extends Message {
 
+	public static final int BROADCAST_SITE_ID = -1;
+
 	public int siteID();
 
 }
